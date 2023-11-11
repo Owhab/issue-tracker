5443215457fb1cc317302c8e935cf922df8a5eb1
Abdul Owhab <owhab.cse@gmail.com>
2023-11-11 23:53:57 +0600

    setting up the radix ui

--- app/issues/page.tsx
+++ app/issues/page.tsx
@@ -1,12 +1,12 @@
+import { Button } from "@radix-ui/themes";
 import React from "react";
 
 const IssuesPage = async () => {
-  const res = await fetch("http://localhost:3000/api/issues");
-  const data = await res.json();
-
-  console.log("data", data);
-
-  return <div>IssuesPage</div>;
+  return (
+    <div>
+      <Button>New Issue</Button>
+    </div>
+  );
 };
 
 export default IssuesPage;
