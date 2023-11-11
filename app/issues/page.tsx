53ce7950279b332b0c06d2845377a71ea4f878c1
Abdul Owhab <owhab.cse@gmail.com>
2023-11-11 23:45:59 +0600

    building api for creating issues

--- app/issues/page.tsx
+++ app/issues/page.tsx
@@ -1,6 +1,11 @@
 import React from "react";
 
-const IssuesPage = () => {
+const IssuesPage = async () => {
+  const res = await fetch("http://localhost:3000/api/issues");
+  const data = await res.json();
+
+  console.log("data", data);
+
   return <div>IssuesPage</div>;
 };
 
